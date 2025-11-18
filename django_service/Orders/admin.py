6352bb9fdf7cb7d3a4cc14a5f5e6fbe1a89b277c
ummad741 <ummad741@gmail.com>
2025-11-18 23:33:58 +0500

    test

--- django_service/Orders/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Order, OrderDetail
-
-# Register your models here.
-
-admin.site.register(Order)
-admin.site.register(OrderDetail)
--- django_service/docker/django.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "django is started"
-
-sh -c "python manage.py runserver 0.0.0.0:8001"
